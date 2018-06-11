9cb059b73ff3fd07655387b31ab8269a9108e4b3
Davy <davenickmalone@gmail.com>
2018-06-11 23:17:12 +0100

    implemented endpoint for creating card object

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
  *  Server entry point
  */
 
-// Node dependencies
+// External dependencies
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // Create Express server
 const app = express();
 
-// Config
+// Configure express to use body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Config variables
 const httpPort = 3000;
 
 // Expost public directory
@@ -16,6 +21,12 @@ app.use(express.static(__dirname + '/public'));
 
 // Define index route
 app.get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+
+// Define create card route
+app.post('/card', (req, res) => {
+  console.log(req.body);
+  res.send('Hello');
+});
 
 // Listen on port
 app.listen(httpPort, () => console.log('Listening on port: ' + httpPort));
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -16,8 +16,28 @@ app.config(function($routeProvider) {
 });
 
 // Define homepage controller
-app.controller('homeController', function($scope) {
-  $scope.title = "Create a motivational card!";
-  $scope.cardTitle = "";
-  $scope.cardMessage = "";
+app.controller('homeController', function($scope, $http) {
+  $scope.title = 'Create a motivational card!';
+  $scope.cardTitle = '';
+  $scope.cardMessage = '';
+
+  $scope.createCard = function() {
+    var req = {
+      method: 'POST',
+      url: '/card',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        title: $scope.cardTitle,
+        message: $scope.cardMessage
+      }
+    };
+
+    $http(req).then(function(success) {
+      console.log(success);
+    }, function(failure) {
+      console.log(failure);
+    });
+  };
 });
